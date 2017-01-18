28f1aa88bb9662860833de02315b704b42fc002e
Hampus Ek <hampusek96@gmail.com>
2017-01-18 14:08:29 +0100

    Added controllers for handling reminders

---
+++ dailyreminder/controllers/ReminderDataController.cs
@@ -0,0 +1,18 @@
+﻿using dailyreminder.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dailyreminder.controllers {
+    abstract class ReminderDataController {
+        public void saveAll(List<Reminder>, string token);
+        public List<Reminder> loadAll(string filepath);
+        public void saveReminder(Reminder reminder);
+        public bool login(string email, string token);
+        public void addNewReminder(Reminder reminder);
+        public void deleteReminder(int reminderId);
+        public void updateReminder(Reminder reminder);
+    }
+}
