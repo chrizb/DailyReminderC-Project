505597205ccc8e3bdc61c3f33a572f8435cc3fa5
Hampus Ek <hampusek96@gmail.com>
2017-01-23 15:19:32 +0100

    Merged the GUI and controllers together

---
+++ dailyreminder/DayImages.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dailyreminder
+{
+    /// <summary>
+    /// Interaction logic for DayImages.xaml
+    /// </summary>
+    public partial class DayImages : UserControl
+    {
+        public DayImages()
+        {
+            InitializeComponent();
+        }
+
+        BitmapImage blueButt = new BitmapImage(new Uri("Images/Buttons/blueButt.png", UriKind.Relative));
+        BitmapImage blueHoverButt = new BitmapImage(new Uri("Images/Buttons/blueHoverButt.png", UriKind.Relative));
+        BitmapImage blueClickedButt = new BitmapImage(new Uri("Images/Buttons/blueClickedButt.png", UriKind.Relative));
+
+        private void dayButt_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+            if (dayButt.Source != blueClickedButt)
+                dayButt.Source = blueClickedButt;
+
+            else dayButt.Source = new BitmapImage(new Uri("Images/frontpageButtPic.png", UriKind.Relative));
+        }
+
+        private void dayButt_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (dayButt.Source != blueClickedButt)
+                dayButt.Source = blueHoverButt;
+        }
+
+        private void dayButt_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (dayButt.Source != blueClickedButt)
+                dayButt.Source = blueButt;
+        }
+    }
+}
--- dailyreminder/controllers/OfflineController.cs
+++ dailyreminder/controllers/OfflineController.cs
@@ -9,20 +9,20 @@ using System.Web.Script.Serialization;
 
 namespace dailyreminder.controllers {
     class OfflineController : ReminderDataController{
-        public void saveAll(List<Reminder> reminderList, string token){
+        public override void saveAll(List<Reminder> reminderList, string token) {
             string path = @"c:\tempFile.dr";
             string json = new JavaScriptSerializer().Serialize(reminderList);
             File.WriteAllText(path, json);
         }
-        public List<Reminder> loadAll(string filepath = @"c:\tempFile.dr") {
+        public override List<Reminder> loadAll(string filepath = @"c:\tempFile.dr") {
             string fileContent = File.ReadAllText(filepath);
             List<Reminder> reminderList = new JavaScriptSerializer().Deserialize<List<Reminder>>(fileContent);
             return reminderList;
         }
-        public void saveReminder(Reminder reminder) { }
-        public bool login(string email, string token) { return true; }
-        public void addNewReminder(Reminder reminder) { }
-        public void deleteReminder(int reminderId) { }
-        public void updateReminder(Reminder reminder) { }
+        public override void saveReminder(Reminder reminder) { }
+        public override bool login(string email, string token) { return true; }
+        public override void addNewReminder(Reminder reminder) { }
+        public override void deleteReminder(int reminderId) { }
+        public override void updateReminder(Reminder reminder) { }
     }
 }
