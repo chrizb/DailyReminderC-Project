a05b94e9fa9416f9cf210d459cd332c701a15d94
Hampus <hampusek96@gmail.com>
2017-01-31 23:21:58 +0100

    Reminders can now be set to done, and it becomes remembered

--- dailyreminder/controllers/AlarmController.cs
+++ dailyreminder/controllers/AlarmController.cs
@@ -22,9 +22,12 @@ namespace dailyreminder.controllers {
 
         List<Alarm> alarms;
 
+        MainController mainController;
+
         DispatcherTimer timer = new DispatcherTimer();
 
-        public AlarmController(List<Reminder> reminders) {
+        public AlarmController(List<Reminder> reminders, MainController mc) {
+            mainController = mc;
             alarms = new List<Alarm>();
             foreach (Reminder reminder in reminders) {
                 alarms.Add(new Alarm(reminder.getEndTimeString(), reminder.Id));
@@ -41,7 +44,14 @@ namespace dailyreminder.controllers {
         private void tickEvent(object sender, EventArgs e) {
             string nowTime = DateTime.Now.Hour + ":" + DateTime.Now.Minute;
             foreach (Alarm alarm in alarms) {
-                if (alarm.Time == nowTime) {
+                Reminder currentReminder = new Reminder { Id = -1 };
+                foreach (Reminder reminder in mainController.getTodaysReminders()) { // Get the specific reminder
+                    if (reminder.Id == alarm.Id) {
+                        currentReminder = reminder;
+                    }
+                }
+
+                if (alarm.Time == nowTime && !currentReminder.Done) {
                     SoundPlayer sp = new SoundPlayer(@"c:\data\foghorn.wav");
                     sp.Play();
                 }
