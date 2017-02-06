626b18772b1265fec25f10ea3053a5bc2c3b49db
hutnelf <ebba1430@hotmail.com>
2017-02-06 15:19:38 +0100

    Fixade dagar

--- dailyreminder/DayImages.xaml.cs
+++ dailyreminder/DayImages.xaml.cs
@@ -29,7 +29,7 @@ namespace dailyreminder
 
         BitmapImage blueButt = new BitmapImage(new Uri("Images/Buttons/blueButt.png", UriKind.Relative));
         BitmapImage blueHoverButt = new BitmapImage(new Uri("Images/Buttons/blueHoverButt.png", UriKind.Relative));
-        BitmapImage blueClickedButt = new BitmapImage(new Uri("Images/Buttons/blueClickedButt.png", UriKind.Relative));
+        BitmapImage blueClickedButt = new BitmapImage(new Uri("Images/Buttons/blueHoverButt.png", UriKind.Relative));
 
         private void dayButt_MouseDown(object sender, MouseButtonEventArgs e)
         {
